7e607d8c0c0a7dd2ea6c0cf7faed3ec6fef99fb9
unknown <m.gambour@hotmail.com>
2022-07-30 15:19:44 +0300

    adding files

---
+++ app/Http/Controllers/ParCodeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Par_code;
+use Illuminate\Http\Request;
+
+class ParCodeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Par_code  $par_code
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Par_code $par_code)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Par_code  $par_code
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Par_code $par_code)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Par_code  $par_code
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Par_code $par_code)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Par_code  $par_code
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Par_code $par_code)
+    {
+        //
+    }
+}
