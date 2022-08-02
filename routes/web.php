ef63c459904f35e7db93109767eee2aa171c6205
unknown <m.gambour@hotmail.com>
2022-08-02 16:54:20 +0300

    adding retrun

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/parcode', [ParCodeController::class, 'index']);
 Route::get('/', [QRController::class, 'index'])->name('qr-form');
 Route::post('/generate-qr-image', [QRController::class, 'generate'])->name('generate-qr-image');
+Route::get('/test', function(){
+    return view('invoice');
+});
 //Route::post('/generate-qr-image', [QRController::class, 'generate'])->name('generate-qr-image');
